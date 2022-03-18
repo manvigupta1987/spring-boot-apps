b933cace897a07400faee267780f3a28d2003d3f
Manvi <manvigupta1987@gmail.com>
2022-03-18 11:37:09 -0400

    microservices-v1:setting up feignclient and ribbon

--- microservices_v1/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
+++ microservices_v1/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -1,5 +1,6 @@
 package com.example.currencyconversionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,15 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyConversionController {
 
+    @Autowired
+    private CurrencyExchangeServiceProxy proxy;
+
     @GetMapping("/currency-conveter/from/{from}/to/{to}/quantity/{quantity}")
     public CurrencyConversion convertCurrency(@PathVariable String from,
                                               @PathVariable String to,
                                               @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+        CurrencyConversion response = proxy.retrieveExchangeValue(from, to);
+        return new CurrencyConversion(response.getId(), from, to, response.getConversionMultiple(),
+                quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
     }
 }
