672f03a1e75e04cfd8ba22aec58b1b56bb661e83
Manvi <manvigupta1987@gmail.com>
2022-03-18 10:27:12 -0400

    microservices-v1:Setup Currency Conversion service

---
+++ microservices_v1/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,18 @@
+package com.example.currencyconversionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conveter/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion convertCurrency(@PathVariable String from,
+                                              @PathVariable String to,
+                                              @PathVariable BigDecimal quantity) {
+        return new CurrencyConversion(1L, from, to, BigDecimal.ONE, quantity, quantity, 0);
+    }
+}
