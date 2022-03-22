6f0480fc9d9e1b7c13ee81d88c4f00ef8aa347bb
Manvi <manvigupta1987@gmail.com>
2022-03-22 05:17:04 -0400

    microservices-v1: eureka naming server changes

--- microservices_v1/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ microservices_v1/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,14 +2,15 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.currencyconversionservice") // package that you want figen to scan
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
-
 }
