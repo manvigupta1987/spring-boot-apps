462d2c21b808283005d5d3f48fdd3f0c4c42f552
Manvi <manvigupta1987@gmail.com>
2022-03-25 05:29:05 -0400

    microservices-v2: Setting up limit service

---
+++ microservices_v2/netflix-zuul-api-gateway/src/main/java/com/example/netflixzuulapigateway/NetflixZuulApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.netflixzuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixZuulApiGatewayApplication.class, args);
+	}
+
+}
