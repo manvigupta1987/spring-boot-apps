808007fcf102a05524c3abd3ad2a4bf8b8a4cf63
Manvi <manvigupta1987@gmail.com>
2022-03-31 06:18:29 -0400

    microservices-v2: configuration for registering the microservices with Eureka

--- microservices_v2/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ microservices_v2/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -3,11 +3,12 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients // package that you want figen to scan
-//@EnableDiscoveryClient
+@EnableEurekaClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
--- microservices_v2/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ microservices_v2/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableEurekaClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
