84d20248f0a6022ec69ef4a40003dea242426447
Manvi <manvigupta1987@gmail.com>
2022-04-05 06:07:55 -0400

    microservices-v2: connecting microservices with GATEWAY

--- microservices_v2/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ microservices_v2/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+	//- http://localhost:8765/CURRENCY-EXCHANGE-SERVICE/currency-exchange/from/USD/to/INR
+	//
+	//- http://localhost:8765/CURRENCY-CONVERSION/currency-conversion-SERVICE/from/USD/to/INR/quantity/10
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
