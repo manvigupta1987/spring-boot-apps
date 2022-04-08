fcaff75e231cbf7587700fb4cf0cf5a537f3c18b
Manvi <manvigupta1987@gmail.com>
2022-04-08 05:37:27 -0400

    micorservices-v2: Log each request using spring cloud

---
+++ microservices_v2/api-gateway/src/main/java/com/example/apigateway/LoggingFilter.java
@@ -0,0 +1,22 @@
+package com.example.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+// Used to log every request that goes through API Gateway
+@Component
+public class LoggingFilter implements GlobalFilter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of the request received: {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
